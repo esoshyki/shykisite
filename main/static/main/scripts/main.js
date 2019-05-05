1c4efef087bef2cfa3eb392716d34899e6c62a8e
esoshyki <esoshyki@yandex.by>
2019-05-05 13:02:41 +0300

    perf: start working with parallax main page

--- main/static/main/scripts/main.js
+++ main/static/main/scripts/main.js
@@ -2,4 +2,11 @@ $('document').ready(function(){
 $('#entertosite').click(function() {
 ("<div>{% include 'login.html' %}</div>").appendTo('#loginhtml')
 })
+});
+
+$(document).ready(function() {
+    var scrollTop = $(window).scrollTop();
+   $('.resume-section').click( function(e) {
+       console.log(scrollTop)
+   })
 })
